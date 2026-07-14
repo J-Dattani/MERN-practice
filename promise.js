164cdd462b52cd46b31270c514f15bcfc552616c
Jaymin Dattani <139145962+J-Dattani@users.noreply.github.com>
2026-07-14 16:21:25 +0530

    Add a basic Promise example with setTimeout

---
+++ promise.js
@@ -0,0 +1,14 @@
+const myPromise = new Promise((resolve, reject) => { 
+  setTimeout(() => { 
+    resolve([7, 4, 1]); 
+    // reject('Things went wrong!'); 
+  }, 2000); 
+});
+
+myPromise
+  .then((result) => { 
+    console.log('Success!', result); 
+  })
+  .catch((error) => { 
+    console.log('Error!', error);
+  });
